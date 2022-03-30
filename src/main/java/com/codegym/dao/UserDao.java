22a2cc462b0395c66fe9feb734f6727edfe758cb
vanh2001 <anhviet1709@gmail.com>
2022-03-30 13:55:38 +0700

    Add file CRUD Prepastatement buoi12

---
+++ src/main/java/com/codegym/dao/UserDao.java
@@ -0,0 +1,152 @@
+package com.codegym.dao;
+
+import com.codegym.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao implements IUserDAO{
+    //Kết nối DB
+    private static final String URLJDBC = "jdbc:mysql://localhost:3306/demoJDBC";
+    private static final String USERJDBC = "root";
+    private static final String PASSWORDJDBC = "12345678";
+
+    //Câu lệnh querry
+    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, email, country) VALUES (?, ?, ?);";
+    private static final String SELECT_USER_BY_ID = "SELECT id,name,email,country FROM users WHERE id =?";
+    private static final String SELECT_All_USERS = "SELECT * FROM users";
+    private static final String DELETE_USERS_SQL = "DELETE FROM users WHERE id = ?;";
+    private static final String UPDATE_USERS_SQL = "UPDATE users SET name = ?,email= ?, country =? WHERE id = ?;";
+
+    public UserDao(){
+    }
+
+    //Lấy kết nối với CSDL
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            //Driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //Connection
+            connection = DriverManager.getConnection(URLJDBC, USERJDBC, PASSWORDJDBC);
+            System.out.println("Ket noi thanh cong");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ket noi chua thanh cong");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    @Override
+    public void insertUser(User user) throws SQLException {
+        System.out.println(SELECT_All_USERS);
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+        ){
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public User selectUser(int id) {
+        User user = null;
+        try(
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);
+                ) {
+            preparedStatement.setInt(1, id);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        List<User> users = new ArrayList<>();
+        try(
+                //Lấy kết nối đến CSDL
+                Connection connection = getConnection();
+                //Chuẩn bị câu lệnh truy vấn
+                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_All_USERS);
+                //Lưu dữ liệu thực thi câu lệnh vào trong ResultSet
+//                ResultSet rs = preparedStatement.executeQuery();
+        ) {
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add( new User(id, name, email, country));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        boolean rowDeleted;
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL);) {
+            preparedStatement.setInt(1, id);
+            rowDeleted = preparedStatement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        boolean rowUpdate;
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL);) {
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
+            preparedStatement.setInt(4, user.getId());
+
+            rowUpdate = preparedStatement.executeUpdate() > 0;
+        }
+        return rowUpdate;
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
